acc9b673dc0f7a074a2cb4f1c72b9378e6f0d023
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-15 10:32:10 -0700

    .env set up properly running nodemon, now to create dummy data

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const ctrl = require('./controller');
+const express = require('express');
+const massive = require('massive');
+const {SERVER_PORT, CONNECTION_STRING} = process.env
+
+
+const app = express()
+
+app.use(express.json())
+
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    app.listen(SERVER_PORT, () => 
+    console.log(`It survived your changes!... ${SERVER_PORT}`)
+    
+)
+
+})
